94f70183a0817bd5aaf4a28845fbdd2ea9d4ad15
Albert <lbrtcoin@gmail.com>
2022-03-17 12:32:01 +0100

    Update section1.js

--- src/components/section1/section1.js
+++ src/components/section1/section1.js
@@ -12,7 +12,7 @@ import secicon from "../../Assets/d.png";
 const Section1 = () => {
   function countLocked() {
     setTimeout(function () {
-      var t = Math.round(Math.random() * (1000000 + 5001) + 627000000);
+      var t = Math.round(Math.random() * (1000000 + 5001) + 21000000);
       document.getElementById("random_number").innerHTML = t;
       countLocked();
     }, 500);
@@ -22,7 +22,7 @@ const Section1 = () => {
 
   function countVolume() {
     setTimeout(function () {
-      var c = Math.round(Math.random() * (1000000 + 52051) + 21000000);
+      var c = Math.round(Math.random() * (1000000 + 521) + 1300);
 
       document.getElementById("random_count").innerHTML = c;
       countVolume();
